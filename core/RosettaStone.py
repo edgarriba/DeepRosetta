ef7cd51fb8075d04f646075d26ff97fcfc77dc3f
prlz77 <pau.rodri1@gmail.com>
2016-07-28 16:26:29 +0200

    Added convert function

--- core/RosettaStone.py
+++ core/RosettaStone.py
@@ -1,9 +1,34 @@
+from importlib import import_module
+import os
+
 class RosettaStone(object):
     """ Class imodeling our network representation
 
     """
-    layer_type = 'empty'
-
     def __init__(self, *args, **kwargs):
-        pass
+        self.importers = self.list_importers()
+        self.exporters = self.list_exporters()
+
+    def list_importers(self):
+        importers = []
+        for d in os.listidr('importers'):
+            if 'Importer' in d:
+                importers.append(d)
+        return importers
+
+    def list_exporters(self):
+        exporters = []
+        for d in os.listidr('exporters'):
+            if 'Exporter' in d:
+                exporters.append(d)
+        return exporters
+
+    def convert(self, input, output, input_format='', output_format=''):
+        assert(input_format in self.importers)
+        assert(output_format in self.exporters)
+        Importer = import_module(os.path.join('io','importers',input_format))
+        Exporter = import_module(os.path.join('io', 'exporters', export_format))
+        Exporter.save(Importer.load(input), output)
+
+
 
